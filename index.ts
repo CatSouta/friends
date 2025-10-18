99bcb46c54ab68ea37ca9f4642c0aaccdf546ec1
CatSouta <sooouta@outlook.com>
2025-10-18 20:18:14 +0800

    feat: 初始化项目

---
+++ index.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import yaml from "js-yaml";
+
+type Friend = {
+  name: string;
+  url: string;
+  img: string;
+  desc: string;
+};
+
+(() => {
+  try {
+    const filePath = "./src/links.yml";
+    const outputPath = "./dist/links.json";
+
+    const file: Friend[] = yaml.load(
+      fs.readFileSync(filePath, "utf-8")
+    ) as Friend[];
+
+    const newFile = file.map((user) => ({
+      ...user,
+      img: `https://i1.stslb.com/souta/friend/${user.img}`,
+    }));
+
+    fs.mkdirSync("./dist", { recursive: true });
+    fs.writeFileSync(outputPath, JSON.stringify(newFile));
+
+    console.log(`File successfully written to ${outputPath}`);
+  } catch (err) {
+    console.error("Error processing file:", err);
+  }
+})();
